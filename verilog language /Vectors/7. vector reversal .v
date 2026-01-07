67538fbe155a7a0f743665cf97494042383f3fa5
BAGIRADHAN S <bagiradhansrinivasan@gmail.com>
2026-01-07 21:55:04 +0530

    Implement vector reversal with and without loop
    Added a vector reversal implementation using a loop.

--- verilog language /Vectors/7. vector reversal .v
+++ verilog language /Vectors/7. vector reversal .v
@@ -1,6 +1,20 @@
+// without loop
 module top_module(
     input  [7:0] in,
     output [7:0] out
 );
     assign out = {in[0], in[1], in[2], in[3], in[4], in[5], in[6], in[7]};
 endmodule
+
+//with loop
+module top_module(
+    input  [7:0] in,
+    output reg [7:0] out
+);
+    integer i;
+    always @(*) begin
+        for (i = 0; i < 8; i = i + 1)
+            out[i] = in[7 - i];
+    end
+endmodule
+
