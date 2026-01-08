ebbf987f6ff9f9b601fd59710196164eb268593c
BAGIRADHAN S <bagiradhansrinivasan@gmail.com>
2026-01-08 20:34:04 +0530

    Add top_module and add1 for 32-bit adder

---
+++ verilog language /Modules : Hierarchy/7.Adder2.v
@@ -0,0 +1,16 @@
+module top_module (
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);
+    wire x;
+    add16 uut1(a[15:0],b[15:0],0,sum[15:0],x);
+    add16 uut2(a[31:16],b[31:16],x,sum[31:16]);
+
+endmodule
+module add1 ( input a, input b, input cin,   output sum, output cout );
+        assign sum=a^b^cin;
+        assign cout=(a&b)|(a&cin)|(b&cin);
+    
+
+endmodule
