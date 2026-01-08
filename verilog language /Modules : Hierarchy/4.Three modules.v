3d0e024d17815b1e601b7a84cc153965f05f6fa8
BAGIRADHAN S <bagiradhansrinivasan@gmail.com>
2026-01-08 20:29:27 +0530

    Add top_module with three D flip-flops

---
+++ verilog language /Modules : Hierarchy/4.Three modules.v
@@ -0,0 +1,16 @@
+module top_module ( input clk, input d, output q );
+    wire x,y;
+    my_dff uut1(
+        .clk(clk),
+        .d(d),
+        .q(x));
+        my_dff uut2(
+            .clk(clk),
+            .d(x),
+            .q(y));
+        my_dff uut3(
+            .clk(clk),
+            .d(y),
+            .q(q));
+
+endmodule
